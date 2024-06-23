6a8694fffbaf5f2fdeb832fdfa1ecefbfbff39f5
GitHubGAllen <134337586+GitHubGAllen@users.noreply.github.com>
2024-06-24 08:45:42 +1200

    hihi

--- Assets/Scripts/RubbishBin.cs
+++ Assets/Scripts/RubbishBin.cs
@@ -7,22 +7,34 @@ public class RubbishBin : MonoBehaviour
 {
     public float trashcount;
     public GameObject trashcounter;
+    public float timer;
+    public bool yes;
     // Start is called before the first frame update
     void Start()
     {
         trashcount = 0;
+        timer = 0;
+        yes = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer = timer+ Time.deltaTime;
        trashcounter.GetComponent<TextMeshPro>().text = "trash count:" + trashcount;
+        if (timer > 1)
+        {
+            yes = true;
+            
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "can")
+        if (other.gameObject.tag == "can" && yes == true)
         {
             trashcount = trashcount + 1;
+            yes = false;
+            timer = 0;
         }
     }
 }
